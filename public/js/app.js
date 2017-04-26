ecfe87f2e96eccdb656ba66e61511f3dce19ea1b
Romain32190 <romain.lafargue36@gmail.com>
2017-04-26 18:56:10 +0200

    Stylisation du jeu

--- public/js/app.js
+++ public/js/app.js
@@ -7,23 +7,13 @@ var Ent = function(x,y,type,regime) {
 
 };
 
-
-Ent.prototype.killMe = function (){
-	for(var i =0; i<entArray.length; i++){
-		if(this === entArray[i]){
-			entArray.splice(i, 1);
-		}
-	}
-};
-
-
 Ent.prototype.vieillir = function() {
     this.age ++;
            if (this.type == "dragon"){
                if (this.age>=15){
                    this.killMe();
            }
-           }else if(this.type == "potager"){
+           }else if(this.type == "paysan"){
                if (this.age>=30){
                    this.killMe();
                }
@@ -34,16 +24,26 @@ Ent.prototype.vieillir = function() {
 
 };
 
-function vieillirall(){
+
+Ent.prototype.killMe = function (){
+	for(var i =0; i<entArray.length; i++){
+		if(this === entArray[i]){
+			entArray.splice(i, 1);
+		}
+	}
+};
+
+function vieillirAll(){
     for (var h = 0; h < entArray.length; h++) {
-        entArray[h].vieillir();
+        	entArray[h].vieillir();
         
     }
 
 }
 
 Ent.prototype.checkAround = function() {
- //var cellAround = [];
+
+// je rajoute les ent a kill dans le tableau Akill[];
  var Akill = [];
  for(var i = 0; i< entArray.length; i++){
  	if(entArray[i].posX>=this.posX-1 && entArray[i].posX <= this.posX+1){
@@ -58,6 +58,8 @@ Ent.prototype.checkAround = function() {
  		}
  	} 
  }
+
+
 
  for(var j = 0; j < Akill.length; j++){
  	Akill[j].killMe();
@@ -82,6 +84,65 @@ function createHtmlArray(w,h){
 
 }
 
+function ajoutPotager(col,lin){    
+   var ent= new Ent(Math.floor(Math.random()*col),Math.floor(Math.random()*lin),"potager","rien");
+   for (var i = 0; i < entArray.length; i++) {
+       if(entArray[i].posX == ent.posX && entArray[i].posY == ent.posY){
+           if (entArray[i].type == "rocher"){
+               return;
+           }else if(entArray[i].type == "dragon"){
+            return;
+        }else if(entArray[i].type == "paysan"){
+            return;
+        }
+           entArray[i] = ent;
+           entArray[i].checkAround();
+           return;
+           // checkAround();
+       }
+   }
+   entArray.push(ent);
+   ent.checkAround();
+     
+}
+
+function ajoutPaysan(col,lin){    
+   var ent= new Ent(Math.floor(Math.random()*col),Math.floor(Math.random()*lin),"paysan","potager");
+   for (var i = 0; i < entArray.length; i++) {
+       if(entArray[i].posX == ent.posX && entArray[i].posY == ent.posY){
+           if (entArray[i].type == "rocher"){
+               return;
+           }else if(entArray[i].type == "dragon"){
+            return;
+        }
+           entArray[i] = ent;
+           entArray[i].checkAround();
+           return;
+           // checkAround();
+       }
+   }
+   entArray.push(ent);
+   ent.checkAround();
+     
+}
+function ajoutRocher(col,lin){    
+   var ent= new Ent(Math.floor(Math.random()*col),Math.floor(Math.random()*lin),"rocher","rien");
+   for (var i = 0; i < entArray.length; i++) {
+       if(entArray[i].posX == ent.posX && entArray[i].posY == ent.posY){
+           
+           entArray[i] = ent;
+           entArray[i].checkAround();
+           return;
+           // checkAround();
+       }
+   }
+   entArray.push(ent);
+   ent.checkAround();
+     
+}
+
+
+
 function affiche(){    
 	$(".cell").html('');
 
@@ -91,8 +152,27 @@ function affiche(){
             var curtype = entArray[i].type;
             var curposX = entArray[i].posX;
             var curposY = entArray[i].posY;
-            var add = $('.cell' + '[data-xy="'+entArray[i].posX+'-'+entArray[i].posY+'"]');
-            $(add).append('<p>'+entArray[i].type+'</p>');
+
+           var add = $('.cell' + '[data-xy="'+entArray[i].posX+'-'+entArray[i].posY+'"]');
+
+            switch (curtype){
+            	case "dragon":
+            		$(add).append('<img class="dragon" ></img>');
+            	break;
+            	case "potager":
+            		$(add).append('<img class="potager" ></img>');
+            	break;
+            	case "rocher":
+            		$(add).append('<img class="rocher" ></img>');
+            	break;
+            	case "paysan":
+            		$(add).append('<img class="paysan" ></img>');
+            	break;
+            }
+
+
+            // var add = $('.cell' + '[data-xy="'+entArray[i].posX+'-'+entArray[i].posY+'"]');
+            // $(add).append('<p>'+entArray[i].type+'</p>');
             //console.log(add);
 
         }
@@ -129,27 +209,26 @@ function game(){
 	        switch(res){
 	        	case "rocher":
 	        		//essayer d'inserer un rocher
-	        		ajoutDragon(10,10);
+	        		ajoutRocher(10,10);
+	        	break;
 
-	        		//lololol
-	        	break;
 	        	case "dragon":
-	        		//
 	        		ajoutDragon(10,10);
 	        	break;
+
 	        	case "paysan":
-	        		ajoutDragon(10,10);
-	        		//
+	        		ajoutPaysan(10,10);
 	        	break;
+
 	        	case "potager":
-	        		ajoutDragon(10,10);
+	        		ajoutPotager(10,10);
 	        	break;
 	        	// default:
 	        	// 	//couille dans le potage
 	        	// break;
 	        }
 	        		affiche();
-	        		vieillirall();
+	        		vieillirAll();
 	    });
 }
 
